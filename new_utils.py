a08921ed01f820b2395d23b071f5996a623213c9
jeremiahbagadion <131178410+jeremiahbagadion@users.noreply.github.com>
2024-02-18 15:38:14 -0500

    Add files via upload

--- new_utils.py
+++ new_utils.py
@@ -10,3 +10,17 @@
   
      import new_utils as nu
 """
+
+import numpy as np
+
+
+# new_utils.py
+def scale_data(X, y):
+    # Ensure X is floating point and scaled between 0 and 1
+    is_scaled = np.all(X >= 0) and np.all(X <= 1)
+    is_floating_point = X.dtype in [np.float32, np.float64]
+    
+    # Ensure y is integer
+    is_integer = y.dtype in [np.int32, np.int64]
+    
+    return is_scaled and is_floating_point and is_integer
